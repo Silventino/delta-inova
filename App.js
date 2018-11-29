726109b08dbd6180c643e3a09c649477e01f6e33
silventino <silventinogato@gmail.com>
2018-11-29 01:06:40 -0200

    commit inicial, inserção funcionando.

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import TextInput from './src/components/TextInput'
+import FormScreen from './src/components/FormScreen'
+
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FormScreen/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2da1d2',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/components/FormScreen.js
@@ -0,0 +1,118 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dimensions from 'Dimensions';
+import TextInput from './TextInput'
+import {StyleSheet, View, Image, TouchableHighlight, KeyboardAvoidingView, Button, Text, TouchableOpacity} from 'react-native';
+
+export default class FormScreen extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+        cnpj: "",
+        nome: "",
+        };
+        
+        this.updateCNPJ = this.updateCNPJ.bind(this)
+        this.updateNome = this.updateNome.bind(this)
+    }
+
+    render() {
+        return (
+        <View style={styles.container}>
+                <Image
+                    source={require('../images/grupodelta-logo.png')}
+                    style={styles.logo}
+                />
+                <Text style={styles.txtCadastro}>
+                    Cadastro de Empresa
+                </Text>
+                <TextInput 
+                    placeholder="Nome da Empresa"
+                    secureTextEntry={false}
+                    autoCorrect={true}
+                    onChangeText = {this.updateNome}
+
+                />
+                <TextInput 
+                    placeholder="CNPJ"
+                    secureTextEntry={false}
+                    autoCorrect={true}
+                    onChangeText = {this.updateCNPJ}
+
+                />
+                <TouchableOpacity style={styles.btnSubmit} onPress={() => this.registerInFirebase(this.state.cnpj, this.state.nome)}>
+                    <Text style={styles.txtButton}>Enviar</Text>
+                </TouchableOpacity>
+
+        </View>
+        );
+    }
+
+    registerInFirebase(){
+        if(this.state.cnpj == "" || this.state.nome == ""){
+            return
+        }
+        console.log(this.state.nome);
+        // fetch('https://jsonplaceholder.typicode.com/todos/1')
+        //     .then(response => response.json())
+        //     .then(json => console.log(json));
+        
+        fetch("https://delta-inova.firebaseio.com/empresas/" + this.state.cnpj + ".json",
+            {
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                method: "PUT",
+                body: JSON.stringify({nome: this.state.nome})
+            })
+            .then(function(res){ console.log(res) })
+            .catch(function(res){ console.log(res) })
+    }
+
+    updateCNPJ(cnpjTxt){
+        this.setState({cnpj : cnpjTxt});
+    }
+    updateNome(txt){
+        this.setState({nome : txt});
+    }
+
+}
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    container: {
+        width: DEVICE_WIDTH -40,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius:10,
+        padding: 10
+    },
+    logo: {
+        height: 120,
+        resizeMode: 'contain',
+        margin: 20,
+        marginBottom: 10
+    },
+    btnSubmit:{
+        padding:5,
+        backgroundColor: '#2da1d2',
+        borderRadius: 20,
+        width: DEVICE_WIDTH/2,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    txtButton:{
+        color: '#fff'
+    },
+    txtCadastro:{
+        fontSize: 18,
+        marginBottom: 10,
+
+    }
+});
